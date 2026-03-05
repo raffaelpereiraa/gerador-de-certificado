656fca2cb278924e3845c36227fc105c47879b93
Rafael Pereira de Moraes <rafael.moraes@IBRAM.GOV>
2026-03-05 16:46:42 -0300

    adicionei o componente primary-button

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
 import { CommonModule } from '@angular/common';
+import { PrimaryButton } from "./components/primary-button/primary-button";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
