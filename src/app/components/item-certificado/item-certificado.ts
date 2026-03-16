fc8ac2ade1fc31021c0c7c2bd100bfd59352541a
raffaelpereiraa <rafaelpereira1324@gmail.com>
2026-03-16 16:49:38 -0300

    usando routerlink para navegação entre paginas

--- src/app/components/item-certificado/item-certificado.ts
+++ src/app/components/item-certificado/item-certificado.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../secondary-button/secondary-button";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: './item-certificado.html',
   styleUrl: './item-certificado.scss',
 })
 export class ItemCertificado {
+
+  id: string = '6';
 
 }
