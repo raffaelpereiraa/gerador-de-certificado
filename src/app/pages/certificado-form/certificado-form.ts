f9704f0de5f152fe6b4fef5b853a0ace5b8b6fd8
raffaelpereiraa <rafaelpereira1324@gmail.com>
2026-03-18 15:51:21 -0300

    Validando os campos do formalario

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from "../../components/primary-button/primary-button";
 import { SecondaryButton } from "../../components/secondary-button/secondary-button";
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [PrimaryButton, SecondaryButton, FormsModule],
+  imports: [PrimaryButton, SecondaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.scss',
 })
@@ -15,4 +16,7 @@ export class CertificadoForm {
   atividade: string ='';
   atividades: string []=['angular', 'react', 'cobol'];
 
+  campoInvalido(control: NgModel){
+    return control.invalid && control.touched
+  }
 }
