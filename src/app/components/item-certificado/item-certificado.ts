c2ee859b305e2949881baae60f0fd0c17d1a345e
raffaelpereiraa <rafaelpereira1324@gmail.com>
2026-03-25 14:59:18 -0300

    listando o historico de certificados gerados

--- src/app/components/item-certificado/item-certificado.ts
+++ src/app/components/item-certificado/item-certificado.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { SecondaryButton } from "../secondary-button/secondary-button";
 import { Router } from "@angular/router";
 
@@ -10,14 +10,20 @@ import { Router } from "@angular/router";
 })
 export class ItemCertificado {
 
-  id: string = '6';
+  @Input () nomeAluno: string = ''
+  @Input () dataEmissao: string = ''
+  @Input () id: string = ''
+
+
+
 
   // Injeção de dependência: Isso permite navegar entre rotas
   constructor(private router: Router) {}
 
   // Router → responsável por navegação entre páginas
   redirecionaCertificado(){
-  this.router.navigate(['/certificados', 2]) //  rota -- /certificados/2
+  this.router.navigate(['/certificados', this.id]) 
+  //  rota -- /certificados/2
   // this.router.navigateByUrl("certificados" + this.id) //  rota -- /certificados/2
 
 }
